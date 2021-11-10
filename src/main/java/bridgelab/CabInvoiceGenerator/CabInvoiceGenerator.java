92225b192c558904f26a34aae6d2c12b25cb94b7
ValarmathyRamachandran <90239548+ValarmathyRamachandran@users.noreply.github.com>
2021-11-10 23:54:45 +0530

    Update CabInvoiceGenerator.java
    Refactored code and tested Total Fare for Premium Rides

--- src/main/java/bridgelab/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/bridgelab/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -2,20 +2,30 @@ package bridgelab.CabInvoiceGenerator;
 
 public class CabInvoiceGenerator {
 
-	private static final double COST_PER_KM = 10;
-    private static final double COST_PER_MINUTE = 1;
-    private static final double MINIMUM_FARE = 5;
+	private static final double NORMAL_RIDE_COST_PER_KM = 10;
+    private static final double NORMAL_RIDE_COST_PER_MINUTE = 1;
+    private static final double NORMAL_RIDE_MINIMUM_FARE = 5;
+    private static final double PREMIUM_RIDE_COST_PER_KM = 15;
+    private static final double PREMIUM_RIDE_COST_PER_MINUTE = 2;
+    private static final double PREMIUM_RIDE_MINIMUM_FARE = 20;
+    
+    
     
     public double calculateFare(double distance, int time) {
-    	double totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
-        return Math.max(totalFare, MINIMUM_FARE);
+    	double totalFare = distance * NORMAL_RIDE_COST_PER_KM + time * NORMAL_RIDE_COST_PER_MINUTE;
+        return Math.max(totalFare, NORMAL_RIDE_MINIMUM_FARE);
     }
     
-    public double calculateFare(Ride[] rides) {
+    public double calculateFareForPremiumRide(double distance, int time) {
+    	double totalFare = distance * PREMIUM_RIDE_COST_PER_KM + time * PREMIUM_RIDE_COST_PER_MINUTE;
+        return Math.max(totalFare, PREMIUM_RIDE_MINIMUM_FARE);
+    }
+    
+    public InvoiceSummary calculateFare(Ride[] rides) {
     	double totalFare = 0;
         for (Ride ride : rides) {
-            totalFare = calculateFare(ride.getDistance(), ride.getTime());
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
         }
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
